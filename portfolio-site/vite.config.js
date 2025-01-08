8b0edbeb633712cb20b929472945fd6c61859b35
Ngee Tian <warkionw12@gmail.com>
2025-01-08 12:25:38 +0800

    Fixing deployment

--- portfolio-site/vite.config.js
+++ portfolio-site/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/Portfolio-Site/" : "/",
+  base: "/Portfolio-Site/",
   plugins: [
     vue(),
     vueDevTools(),
